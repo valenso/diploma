76c2165253bcaab46704c05450fd6b3cda45c030
Valentin <mvalenso@gmail.com>
2014-04-14 00:19:16 +0400

    Adding a folder for the database

--- js/build/production.js
+++ js/build/production.js
@@ -1,16 +1,3 @@
-var poligon = 
-{
-	"name" : "component1",
-	"articul" : 1,
-	"coordinates" : [
-					[10,10],
-					[20,20],
-					[100,10],
-					[100,100],
-					[10,100],
-					[10,10]
-					]
-};
 app.defaultStroked = true;
 function drawItem(item) {
 	newPath = app.activeDocument.pathItems.add(); 
