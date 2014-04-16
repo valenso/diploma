4dbd966a4cc3a1579f15fc9937d947a3150e1990
Valentin <mvalenso@gmail.com>
2014-04-16 09:05:57 +0400

    inscribed in a rectangle

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ if ( app.documents.length > 0 ) {
 	drawItem(component);
 	moveItem(component,-100,-200);
 	drawItem(component);
+	zoomItem(component);
 }
--- js/objectchanges.js
+++ js/objectchanges.js
@@ -9,3 +9,30 @@ function moveItem(item,x,y){
 
 	return true;
 };
+
+function zoomItem(item, zoom){
+	zoom = typeof zoom !== 'undefined' ? zoom : 1;
+
+	var maxX = item.coordinates[0][0];
+	var minX = item.coordinates[0][0];
+	var maxY = item.coordinates[0][1];
+	var minY = item.coordinates[0][1];
+
+	for (var index = 0; index < item.coordinates.length; ++index) {
+    	if (maxX < item.coordinates[index][0]){
+    		maxX = item.coordinates[index][0];
+    	}
+    	else if (minX > item.coordinates[index][0]){
+    		minX = item.coordinates[index][0];
+    	}
+    	if (maxY < item.coordinates[index][1]){
+    		maxY = item.coordinates[index][1];
+    	}
+    	else if (minY > item.coordinates[index][1]){
+    		minY = item.coordinates[index][1];
+    	}
+	}
+
+	newPath = app.activeDocument.pathItems.add(); 
+	newPath.setEntirePath([[maxX,maxY],[maxX,minY],[minX,minY],[minX,maxY],[maxX,maxY]]);
+}
