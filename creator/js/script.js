30ad95eb3916d6d13b62f32a8a9b1ea6c7dc3a26
Valentin <mvalenso@gmail.com>
2014-04-23 01:06:55 +0400

    drag and drop upload image

--- creator/js/script.js
+++ creator/js/script.js
@@ -1,8 +1,59 @@
-var canvas = document.getElementById('myCanvas');
-var context = canvas.getContext('2d');
-var imageObj = new Image();
+document.addEventListener("DOMContentLoaded", init, false);
 
-imageObj.onload = function() {
-	context.drawImage(imageObj, 69, 50);
-};
-imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
+function writeMessage(canvas, message) {
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = '18pt Calibri';
+    context.fillStyle = 'black';
+    context.fillText(message, 10, 25);
+}
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+    	x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+function addPosition(message) {
+	var text = document.getElementById('text');
+	text.value += message + ',\n';
+}
+
+function init()
+    {
+        var canvas = document.getElementById('myCanvas');
+		var context = canvas.getContext('2d');
+		img = document.createElement("img");
+
+		canvas.addEventListener("dragover", function (evt) {
+			evt.preventDefault();
+		}, false);
+
+		canvas.addEventListener("drop", function (evt) {
+			var files = evt.dataTransfer.files;
+			if (files.length > 0) {
+				var file = files[0];
+				if (typeof FileReader !== "undefined" && file.type.indexOf("image") != -1) {
+					var reader = new FileReader();
+					reader.onload = function (evt) {
+						img.src = evt.target.result;
+					};
+					reader.readAsDataURL(file);
+				}
+			}
+			evt.preventDefault();
+		}, false);
+        
+        canvas.addEventListener('mousedown', function(evt){
+			var mousePos = getMousePos(canvas, evt);
+    		var message = '[' + mousePos.x + ',' + mousePos.y + ']';
+    		addPosition(message);
+		}, false);
+
+		img.addEventListener("load", function () {
+			//clearCanvas();
+			context.drawImage(img, 0, 0, canvas.width, canvas.height);
+		}, false);
+    }
