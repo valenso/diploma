c18aad8f97a892c31276e8294e4594aecd0aea5a
Valentin <mvalenso@gmail.com>
2014-05-27 10:55:36 +0400

    27 мая

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-	var element = component;
+	/*var element = component;
 	drawItem(element);
 	zoomItem(element, 1.5);
 	drawItem(element);
 	drawConturItem(element);
+
+	var element = osnov;
+	drawItem(element);*/
+
+var aiFile = File('~/Documents/diploma/April-version/js/build/test.ai');
+app.open(aiFile);
